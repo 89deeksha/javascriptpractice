ffb81b5554f799b3c9726691a617ea0aeb4aa7e1
89deeksha <deekshajain0310@gmail.com>
2025-02-07 14:15:56 +0530

    constructorfunction

--- script.js
+++ script.js
@@ -42,3 +42,53 @@ console.log(key)
 for(let key in person){
 console.log(key +":" +person[key]) //for access values from object
 }
+//Action or method
+person.greet=function(){
+	console.log('hello')
+}
+person.greet(); //now the object is ready to say hello
+//Another way to perform action
+function greetings(){
+	console.log("helllo!")
+}
+person.fun=greetings;
+person.fun();
+let cars={
+	brand:'TATA',
+	model:'Safari'
+}
+let people={
+	name:'Robert', //property
+	Age:'21', //property
+	Car(){console.log(people.name +' have'+" "+cars.brand+'with model'+cars.model) //methods
+	}
+}
+people.Car()
+
+let num=Math.floor(Math.random()*10) //this will show numbers from 0-9 
+console.log(num)
+function getRandom(min, max){
+	let x=Math.floor(Math.random()*(max-min))
+	return x;
+}
+getRandom(3,10)
+var books={
+	name:"science",
+	auther:"sp verma",
+	set modifyname(n){
+this.name=n.toUpperCase();
+	}
+}
+books.modifyname='deeksha jain'
+console.log(books)
+//constructor function
+function student(cls,age,name,surname){
+	this.firstName=name,
+	this.lastName=surname,
+	this.age=age,
+	this.class=cls
+}
+var student1=new student(12,22,"Deeksha","Jain")
+var student2=new student(22,20,"Priya","sharma")
+console.log(student1,student2)
+
